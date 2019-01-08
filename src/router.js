c3bebeb17e9ce0b55f5363157313c9f156570210
Xu Zhi Wei <1304348418@qq.com>
2019-01-08 15:19:07 +0800

    first commit

--- src/router.js
+++ src/router.js
@@ -1,25 +1,70 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/index',
+      name: 'index',
+      component:resolve => require(['@/components/index.vue'],resolve)
+      // component: () => import(/* webpackChunkName: "index" */ './components/index.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      path: '/',
+      name: 'login',
+      component: () => import(/* webpackChunkName: "login" */ './components/login.vue')
+    },
+    {
+      path: '/silkBag/:tipid',
+      name: 'silkBag',
+      component: () => import(/* webpackChunkName: "silkBag" */ './components/silkBag/silkBag.vue')
+    },
+    {
+      path: '/twoLevel/:tipid',
+      name: 'twoLevel',
+      component: () => import(/* webpackChunkName: "twoLevel" */ './components/silkBag/twoLevel.vue')
+    },
+    {
+      path: '/details/:tipid',
+      name: 'details',
+      component: () => import(/* webpackChunkName: "details" */ './components/silkBag/details.vue')
+    },
+    {
+      path: '/sleceted',
+      name: 'sleceted',
+      component: () => import(/* webpackChunkName: "sleceted" */ './components/sleceted/sleceted.vue')
+    },
+    {
+      path: '/slecetedDetails/:selectid',
+      name: 'slecetedDetails',
+      component: () => import(/* webpackChunkName: "slecetedDetails" */ './components/sleceted/details.vue')
+    },
+    {
+      path: '/mapbox/:coordinateX/:coordinateY',
+      name: 'mapbox',
+      component: () => import(/* webpackChunkName: "mapbox" */ './components/common/mapbox.vue')
+    },
   ]
 })
+router.beforeEach((to, from, next) => {
+  const whiteList = ['/silkBag','/twoLevel','/details','/sleceted','/slecetedDetails','/mapbox'] // 路由白名单
+  const token = sessionStorage.getItem('token')
+  if (token) {
+    next();
+  } else {
+    if (whiteList.indexOf(to.path) !== -1) { // 在免登录白名单，直接进入
+      next();
+    } else {
+      if (to.path === '/') {
+        next()
+      } else {
+        next('/')
+      }
+    }
+  }
+});
+export default router
